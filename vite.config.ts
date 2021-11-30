9f787521174f31236743252c78881709e3db1896
CaptainOfPhB <captaininphw@gmail.com>
2021-12-01 00:30:13 +0800

    add eslint config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,22 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
     rollupOptions: {
-      input: resolve(__dirname, 'playground/index.html'),
-    },
+      input: resolve(__dirname, 'playground/index.html')
+    }
   },
   server: {
     open: '/playground/index.html'
   },
   plugins: [
     react(),
-  ],
+    eslint({
+      throwOnError: true,
+      throwOnWarning: true
+    })
+  ]
 });
