1ebb120b07223717b595194221907614fd0352cc
CaptainOfPhB <captaininphw@gmail.com>
2021-12-01 00:31:03 +0800

    add Text component

---
+++ src/Text/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Form, Input } from 'antd';
+
+interface TextProps {
+  label: React.ReactNode;
+  placeholder?: string;
+}
+
+function Text(props: TextProps) {
+  return (
+    <Form.Item label={props.label}>
+      <Input placeholder={props.placeholder} />
+    </Form.Item>
+  );
+}
+
+export default Text;
