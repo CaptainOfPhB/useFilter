054992e4488adc8968175002ce143a6a7e64b0b0
CaptainOfPhB <captainofphb@gmail.com>
2021-12-01 16:57:12 +0800

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react';
+import typescript from '@rollup/plugin-typescript';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   build: {
+    outDir: 'lib',
+    polyfillModulePreload: false,
+    lib: {
+      formats: ['es'],
+      entry: resolve(__dirname, 'components/index.ts')
+    },
     rollupOptions: {
-      input: resolve(__dirname, 'playground/index.html')
+      external: ['react', 'react-dom', 'object-assign', 'antd']
     }
   },
   server: {
@@ -14,9 +22,16 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    typescript({
+      tsconfig: 'tsconfig.build.json'
+    }),
+    visualizer({
+      open: true
+    }),
     eslint({
       throwOnError: true,
-      throwOnWarning: true
+      throwOnWarning: true,
+      include: ['src', 'playground']
     })
   ]
 });
