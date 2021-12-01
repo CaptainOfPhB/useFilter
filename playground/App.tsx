73e79006ac5f31458c8eb4ab121cff42a83ebceb
CaptainOfPhB <captaininphw@gmail.com>
2021-12-01 23:33:21 +0800

    update demo

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
+import { Card } from 'antd';
+import Filter, { Text } from '../components';
 
 function App() {
-  return <div>test demo</div>;
+  const onSearch = () => new Promise<boolean>(resolve => setTimeout(() => resolve(true), 2000));
+  const onReset = () => console.log('onReset');
+  const onChange = () => console.log(123);
+  return (
+    <div>
+      <Card title={Text.name}>
+        <Filter onSearch={onSearch} onReset={onReset}>
+          <Text label='Text demo' onChange={onChange} />
+        </Filter>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
