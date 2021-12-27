608a67fe7a876daa6ae90cb3946d509629673821
CaptainOfPhB <captaininphw@gmail.com>
2021-12-27 21:35:13 +0800

    remove unused file

--- src/create.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { FormInstance } from 'antd';
-
-export default function create<P extends { form: FormInstance }>(Component: React.ComponentType<P>) {
-  return function (props: Omit<P, 'form'>): JSX.Element {
-    return <Component {...(props as P)} />;
-  };
-}
