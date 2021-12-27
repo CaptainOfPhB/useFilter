8763a9de7b92244160f431350239a1d95d60c6dc
CaptainOfPhB <captaininphw@gmail.com>
2021-12-28 00:03:55 +0800

    add Text.spec.ts

---
+++ src/Text/Text.spec.ts
@@ -0,0 +1,9 @@
+import '@testing-library/jest-dom';
+import { renderHook } from '@testing-library/react-hooks';
+import { render } from '@testing-library/react';
+
+import Text from './index';
+import useFilter from '../useFilter';
+
+test('should render to screen', () => {
+});
