6a58cdf9baff57be39ca7d94e98687570b4c9393
CaptainOfPhB <captaininphw@gmail.com>
2021-12-27 23:15:51 +0800

    remove unused code

--- src/Text/index.tsx
+++ src/Text/index.tsx
@@ -2,45 +2,10 @@ import React from 'react';
 import { Form, Input } from 'antd';
 import { BaseProps, TextBaseProps } from '../interface';
 
-export interface TextProps<V> extends TextBaseProps, BaseProps<V> {
-  trim?: boolean;
-}
+export type TextProps<V> = TextBaseProps & BaseProps<V>;
 
 function Text<Values>(props: TextProps<Values>) {
   const { textProps, fieldProps } = props;
-
-  // const [value, setValue] = useState<string | undefined>(propsValue);
-
-  // useEffect(() => {
-  //   if (propsValue === undefined) {
-  //     setStatus('');
-  //     setHelp(undefined);
-  //   }
-  // }, [propsValue]);
-
-  // const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   setStatus('');
-  //   setValue(e.target.value);
-  //   setHelp(undefined);
-
-  //   const trimmedValue = e.target.value.trim();
-
-  //   if (!trimmedValue) {
-  //     onChange(undefined);
-  //     return;
-  //   }
-
-  //   if (validator) {
-  //     const result = validator(trimmedValue);
-  //     if (result !== true) {
-  //       setHelp(result);
-  //       setStatus('error');
-  //       return;
-  //     }
-  //   }
-
-  //   normalizer ? onChange(normalizer(trimmedValue)) : onChange(trimmedValue);
-  // };
 
   return (
     <Form.Item
