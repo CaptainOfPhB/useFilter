8e657cf2d4c4706acdf63aef507b938f8ce5cb7d
CaptainOfPhB <captaininphw@gmail.com>
2022-01-01 23:32:49 +0800

    create setupTest and mock matchMedia

---
+++ mock/matchMedia.ts
@@ -0,0 +1,15 @@
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    media: query,
+    matches: false,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn()
+  }))
+});
+
+export {};
