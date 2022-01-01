c6b0221378121667ee3838e52b18333988172ffb
CaptainOfPhB <captaininphw@gmail.com>
2022-01-01 23:35:51 +0800

    complete props type

--- src/useFilter.tsx
+++ src/useFilter.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Form } from 'antd';
-import { ReactElement } from 'react';
 import Filter, { FilterProps } from './Filter';
-import { FormInstance } from 'antd/lib/form/Form';
-
-export type ChildType = ReactElement<{ span?: number; form: FormInstance }>;
 
 function useFilter<FilterValues>() {
   const [form] = Form.useForm();
   return {
-    Filter: (filterProps: Omit<FilterProps<FilterValues>, 'form'>) => <Filter form={form} {...filterProps} />,
+    Filter: (props: React.PropsWithChildren<Omit<FilterProps<FilterValues>, 'form'>>) => {
+      const { children, ...rest } = props;
+      return (
+        <Filter form={form} {...rest}>
+          {children}
+        </Filter>
+      );
+    },
     reset: form.resetFields,
     validate: form.validateFields,
     getValue: form.getFieldValue,
