0b95a2da96e68939a0b4d37a1df220ca43d06cfa
CaptainOfPhB <captainofphb@gmail.com>
2022-01-04 16:07:49 +0800

    add return type

--- src/useFilter.tsx
+++ src/useFilter.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Form } from 'antd';
 import Filter, { FilterProps } from './Filter';
+import { FormInstance } from 'antd/lib/form/Form';
 
-function useFilter<FilterValues>() {
+export type Return<V> = {
+  Filter: (props: React.PropsWithChildren<Omit<FilterProps<V>, 'form'>>) => JSX.Element;
+  reset: FormInstance<V>['resetFields'];
+  validate: FormInstance<V>['validateFields'];
+  getValue: FormInstance<V>['getFieldValue'];
+  getValues: FormInstance<V>['getFieldsValue'];
+  setValues: FormInstance<V>['setFieldsValue'];
+};
+
+function useFilter<FilterValues>(): Return<FilterValues> {
   const [form] = Form.useForm();
   return {
     Filter: (props: React.PropsWithChildren<Omit<FilterProps<FilterValues>, 'form'>>) => {
