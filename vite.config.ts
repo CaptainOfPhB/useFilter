c8c17aeac01991bd0c5202fd31cedca508d2a2f9
CaptainOfPhB <captainofphb@gmail.com>
2022-01-04 16:05:27 +0800

    add mode condition

--- vite.config.ts
+++ vite.config.ts
@@ -5,35 +5,37 @@ import react from '@vitejs/plugin-react';
 import typescript from '@rollup/plugin-typescript';
 import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  build: {
-    outDir: 'lib',
-    polyfillModulePreload: false,
-    lib: {
-      formats: ['es'],
-      entry: resolve(__dirname, 'src/index.ts')
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      outDir: 'lib',
+      polyfillModulePreload: false,
+      lib: {
+        formats: ['es'],
+        entry: resolve(__dirname, 'src/index.ts')
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom', 'antd']
+      }
     },
-    rollupOptions: {
-      external: ['react', 'react-dom', 'antd']
-    }
-  },
-  server: {
-    hmr: false,
-    open: 'playground/index.html'
-  },
-  plugins: [
-    react(),
-    typescript({
-      tsconfig: 'tsconfig.json'
-    }),
-    visualizer({
-      open: true
-    }),
-    eslint({
-      cache: false,
-      throwOnError: true,
-      throwOnWarning: true,
-      include: ['src/**/*.ts', 'src/**/*.tsx', 'playground/**/*.ts', 'playground/**/*.tsx']
-    })
-  ]
+    server: {
+      hmr: false,
+      open: '/playground/index.html'
+    },
+    plugins: [
+      react(),
+      typescript({
+        tsconfig: mode === 'development' ? 'tsconfig.json' : 'tsconfig.build.json'
+      }),
+      visualizer({
+        open: true
+      }),
+      eslint({
+        cache: false,
+        throwOnError: true,
+        throwOnWarning: true,
+        include: ['src/**/*.ts', 'src/**/*.tsx', 'playground/**/*.ts', 'playground/**/*.tsx']
+      })
+    ]
+  };
 });
