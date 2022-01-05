64e2331af0b5248c19aa9f20b5618ca9653aac8c
CaptainOfPhB <captainofphb@gmail.com>
2022-01-05 23:41:31 +0800

    create Selector

---
+++ src/Selector/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { SelectValue } from 'antd/lib/select';
+import { Form, Select, SelectProps } from 'antd';
+import { FieldProps, PruneProps } from '../index';
+
+type SelectorKeys =
+  | 'loading'
+  | 'notFoundContent'
+  | 'optionFilterProp'
+  | 'optionLabelProp'
+  | 'options'
+  | 'showSearch'
+  | 'onSearch'
+  | 'disabled'
+  | 'placeholder'
+  | 'onChange'
+  | 'allowClear'
+  | 'fieldNames'
+  | 'filterOption'
+  | 'labelInValue';
+
+export type SelectorProps<V> = PruneProps<SelectProps<V>, SelectorKeys, 'extras'>;
+
+function Selector<Value extends SelectValue, FieldsValue = unknown>(
+  props: SelectorProps<Value> & FieldProps<FieldsValue>
+) {
+  const { extras, fields } = props;
+
+  return (
+    <Form.Item
+      {...fields}
+      name={props.name}
+      rules={props.rules}
+      label={props.label}
+      extra={props.extra}
+      tooltip={props.tooltip}
+      required={props.required}
+      normalize={props.normalize}
+      initialValue={props.initialValue}
+    >
+      <Select<Value>
+        {...extras}
+        onChange={props.onChange}
+        disabled={props.disabled}
+        allowClear={props.allowClear}
+        placeholder={props.placeholder}
+      />
+    </Form.Item>
+  );
+}
+
+export default Selector;
