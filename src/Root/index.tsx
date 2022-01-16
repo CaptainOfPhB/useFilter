c3743780095a6b9c33fcd8109f60136e8702d00d
CaptainOfPhB <captainofphb@gmail.com>
2022-01-16 23:08:43 +0800

    refactor Root

--- src/Root/index.tsx
+++ src/Root/index.tsx
@@ -1,35 +1,41 @@
-import React, { useCallback } from 'react';
-import { RowProps } from 'antd/lib/grid/row';
+import { Button, Col, Form, Row, Space } from 'antd';
+import { UpOutlined, DownOutlined } from '@ant-design/icons';
 import { FormInstance, FormProps } from 'antd/lib/form/Form';
-import { Button, Col, ColProps, Form, Row, Space } from 'antd';
-import { ReactElement, Children, useState, useMemo } from 'react';
+import { PropsWithChildren, ReactNode, useMemo } from 'react';
+import React, { useState, useCallback, Children } from 'react';
 
-export type ChildType = ReactElement<{ span?: number }>;
-
-export interface RootProps<V> {
-  defaultSpan?: number;
-  actionProps?: ColProps;
-  gap?: RowProps['gutter'];
-  resetText?: React.ReactNode;
-  searchText?: React.ReactNode;
-  customAction?: React.ReactNode;
-  form: FormInstance<V>;
-  children: ChildType | ChildType[];
-  filterProps?: Omit<FormProps<V>, 'form'>;
-  onSubmit?: (fieldsValue: V) => Promise<unknown> | void;
+export enum Column {
+  default = 6,
+  large = 3
 }
 
-function Root<V>(props: RootProps<V>) {
-  const defaultSpan = props.defaultSpan || 4;
-  const [loading, setLoading] = useState<boolean | { delay?: number }>(false);
-  const { form, onSubmit, filterProps, actionProps, customAction, searchText, resetText } = props;
+export enum Span {
+  default = 4,
+  large = 8
+}
 
-  const ColWrappedChildren = useMemo(() => {
-    return Children.map(props.children, function (child: ChildType) {
-      const span = child.props.span || defaultSpan;
-      return <Col span={span}>{child}</Col>;
-    });
-  }, [props.children, defaultSpan]);
+export enum Gutter {
+  default = 16,
+  large = 24
+}
+
+export interface RootProps<V> {
+  form: FormInstance<V>;
+  defaultExpanded?: boolean;
+  size?: 'default' | 'large';
+  resetText?: React.ReactNode;
+  submitText?: React.ReactNode;
+  rest?: Omit<FormProps<V>, 'form'>;
+  onSubmit?: (values: V) => Promise<unknown> | void;
+}
+
+function Root<V>(props: PropsWithChildren<RootProps<V>>) {
+  const span = props.size ? Span[props.size] : Span.default;
+  const gutter = props.size ? Gutter[props.size] : Gutter.default;
+  const columns = props.size ? Column[props.size] : Column.default;
+  const [loading, setLoading] = useState<boolean | { delay?: number }>(false);
+  const [expanded, setExpanded] = useState<boolean>(props.defaultExpanded || false);
+  const { form, onSubmit, children, rest, submitText: searchText, resetText } = props;
 
   const onFinish = useCallback(
     async (values: V) => {
@@ -45,22 +51,30 @@ function Root<V>(props: RootProps<V>) {
     [onSubmit]
   );
 
+  const [memoizedOffset, memoizedChildren] = useMemo(() => {
+    const pickedChildren = expanded ? children : Children.toArray(children).slice(0, columns - 1);
+    const childrenLength = Children.count(pickedChildren);
+    const column = columns - (childrenLength % columns) - 1;
+    const colWrappedChildren = Children.map(pickedChildren, (child: ReactNode) => <Col span={span}>{child}</Col>);
+    return [column * span, colWrappedChildren];
+  }, [children, columns, expanded, span]);
+
   return (
-    <Form form={form} layout='vertical' {...filterProps} onFinish={onFinish}>
-      <Row gutter={props.gap || 16}>
-        {ColWrappedChildren}
-        <Col span={defaultSpan} {...actionProps}>
+    <Form form={form} layout='vertical' onFinish={onFinish} {...rest}>
+      <Row gutter={gutter}>
+        {memoizedChildren}
+        <Col offset={memoizedOffset} span={span} style={{ textAlign: 'right' }}>
           <Form.Item label=' '>
-            {customAction || (
-              <Space size='middle'>
-                <Button type='primary' htmlType='submit' loading={loading}>
-                  {searchText || '搜索'}
-                </Button>
-                <Button type='dashed' htmlType='reset'>
-                  {resetText || '重置'}
-                </Button>
-              </Space>
-            )}
+            <Space>
+              <Button type='primary' htmlType='submit' loading={loading}>
+                {searchText || '搜索'}
+              </Button>
+              <Button htmlType='reset'>{resetText || '重置'}</Button>
+              <Button type='link' onClick={() => setExpanded(!expanded)}>
+                {expanded ? '收起' : '展开'}
+                {expanded ? <UpOutlined /> : <DownOutlined />}
+              </Button>
+            </Space>
           </Form.Item>
         </Col>
       </Row>
