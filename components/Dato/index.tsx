456ced391bae4d446d6f26109c12bf7513641fb7
CaptainOfPhB <captainofphb@gmail.com>
2022-02-09 22:46:35 +0800

    update Dato component

--- components/Dato/index.tsx
+++ components/Dato/index.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Prune } from '../index';
 import Field, { FieldProps } from '../Field';
 import { DatePicker, DatePickerProps } from 'antd';
 
 const defaultFormat = 'YYYY-MM-DD HH:mm:ss';
 
-export type DatoProps = DatePickerProps;
+type DatoKeys = 'disabled' | 'placeholder' | 'allowClear' | 'disabledDate' | 'format' | 'onChange';
+
+export type DatoProps = Prune<DatePickerProps, DatoKeys, 'rest'>;
 
 function Dato(props: DatoProps & FieldProps) {
   return (
     <Field {...props}>
-      <DatePicker format={defaultFormat} style={{ width: '100%' }} />
+      <DatePicker
+        style={{ width: '100%' }}
+        disabled={props.disabled}
+        onChange={props.onChange}
+        allowClear={props.allowClear}
+        placeholder={props.placeholder}
+        disabledDate={props.disabledDate}
+        format={props.format || defaultFormat}
+      />
     </Field>
   );
 }
